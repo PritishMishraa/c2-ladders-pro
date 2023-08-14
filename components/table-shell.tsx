3d8a9a620fff605ca9bbf84ebc3738e8fd788b7f
Pritish Mishra <pritishmishra579@gmail.com>
2023-08-14 13:44:07 +0530

    feat: :sparkles: shell for data table

---
+++ components/table-shell.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React from "react";
+import { z } from "zod";
+import { Metadata } from "next";
+
+import { ProblemStatus } from "@/types/types";
+import { columns } from "./data-table/columns";
+import { DataTable } from "./data-table/data-table";
+import { Question, questionSchema } from "./data/schema";
+
+export const metadata: Metadata = {
+  title: "Questions",
+  description: "A task and issue tracker build using Tanstack Table.",
+};
+
+async function getQuestions(rating: number) {
+  const response = await fetch(
+    `https://c2-ladders.com/api/ladder?startRating=${rating}&endRating=${
+      rating + 100
+    }`
+  );
+
+  let { data: questions }: { data: Question[] } = await response.json();
+
+  questions = questions.map((question) => {
+    return { ...question, status: ProblemStatus.NONE };
+  });
+
+  return z.array(questionSchema).parse(questions);
+}
+
+interface TableShellProps {
+  rating: number;
+}
+
+export function TableShell({ rating }: TableShellProps) {
+  const [questions, setQuestions] = React.useState<Question[]>([]);
+
+  React.useEffect(() => {
+    async function fetchQuestions() {
+      const newQuestions = await getQuestions(rating);
+      setQuestions(newQuestions);
+    }
+
+    fetchQuestions();
+  }, [rating]);
+
+  return <DataTable data={questions} columns={columns} />;
+}
