103b5d75be3f7f0c82e71c36aa106513ca07c343
Pritish Mishra <pritishmishra579@gmail.com>
2023-08-14 13:38:14 +0530

    feat: :chart_with_upwards_trend: microsoft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ideknod751");
+            `,
+          }}
+        ></script>
+      </head>
       <body
         className={
           (cn(inter.className), "min-h-screen bg-background antialiased")
