863c3782d85cb6ad0496fff25e22fa73cdc2c836
Pritish Mishra <pritishmishra579@gmail.com>
2023-08-16 12:55:35 +0530

    feat: :sparkles: data table loading state

--- components/table-shell.tsx
+++ components/table-shell.tsx
@@ -7,6 +7,7 @@ import { ProblemStatus } from "@/types/types";
 import { columns } from "./data-table/columns";
 import { DataTable } from "./data-table/data-table";
 import { Question, questionSchema } from "./data/schema";
+import { DataTableLoading } from "./data-table/data-table-loading";
 
 export const metadata: Metadata = {
   title: "Questions",
@@ -14,7 +15,9 @@ export const metadata: Metadata = {
 };
 
 async function getQuestions(rating: number) {
-  const response = await fetch(`/api/question?rating=${rating}`);
+  const response = await fetch(`/api/question?rating=${rating}`, {
+    cache: "no-cache",
+  });
 
   let { data: questions }: { data: Question[] } = await response.json();
 
@@ -30,16 +33,21 @@ interface TableShellProps {
 }
 
 export function TableShell({ rating }: TableShellProps) {
+  const [isLoading, setIsLoading] = React.useState(true);
   const [questions, setQuestions] = React.useState<Question[]>([]);
 
   React.useEffect(() => {
+    setIsLoading(true);
     async function fetchQuestions() {
       const newQuestions = await getQuestions(rating);
       setQuestions(newQuestions);
     }
 
     fetchQuestions();
+    setIsLoading(false);
   }, [rating]);
+
+  if (isLoading) return <DataTableLoading columnCount={4} />;
 
   return <DataTable data={questions} columns={columns} />;
 }
